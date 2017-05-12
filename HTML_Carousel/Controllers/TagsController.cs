dac0ccd6426bb8306e57893ea513650a07006f13
Konnor Welsch <konnor.welsch@eagles.ewu.edu>
2017-05-11 22:42:41 -0700

    Initial setup of project and site layout

---
+++ HTML_Carousel/Controllers/TagsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using HTML_Carousel.Models;
+
+namespace HTML_Carousel.Controllers
+{
+    public class TagsController : Controller
+    {
+        private TagList _tagList = new TagList();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult a()
+        {
+            return View();
+        }
+    }
+}
